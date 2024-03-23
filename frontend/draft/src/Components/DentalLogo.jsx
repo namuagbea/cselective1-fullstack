d97e7a6c3665786a69c3bec80fdb900e8e066d1a
MS-Crexa <vthelym@gmail.com>
2024-03-23 14:53:32 +0800

    Folder organized.

---
+++ frontend/draft/src/Components/DentalLogo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+const DentalLogo = () => {
+  return (
+   <div className="h-20 w-20">
+    <img
+     src="https://github.com/namuagbea/cselective1-fullstack/blob/main/assets/SmilePrimeLgo.png?raw=true"
+     alt="Logo"
+    />
+   </div>
+  );
+}
+
+export default DentalLogo;
