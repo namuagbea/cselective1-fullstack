05f9400fe657378aa9c72778f16fa3ae455a6c6b
MS-Crexa <vthelym@gmail.com>
2024-03-23 15:15:01 +0800

    Organized Folder

--- frontend/draft/src/Components/Services.jsx
+++ frontend/draft/src/Components/Services.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div className='bg-red-500'>Services</div>
-  )
+   <div className="bg-[#224F79]">
+    <div className='pt-20 ml-20'>
+     <span>Welcome to</span>
+     <span className="text-[#52CCB6]">Smile Prime</span>
+    </div>
+   </div>
+  );
 }
 
 export default Services
