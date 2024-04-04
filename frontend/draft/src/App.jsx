5c6c55f92f439ba968e31ac193e458a5f69363a9
MS-Crexa <vthelym@gmail.com>
2024-04-04 10:04:47 +0800

    Smooth scroll and organized folder

--- frontend/draft/src/App.jsx
+++ frontend/draft/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import "./App.css";
-import LandingPage from './Pages/LandingPage.jsx';
+import LandingPage from './Pages/LandingPage/LandingPage';
 
 
 
 export default function App() {
   return (
     <>
-    <LandingPage/>
-    
+      <LandingPage />
+
+
     </>
   );
 }
---
+++ frontend/draft/src/Pages/LandingPage/Components/Login.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import SignUp from '../Components/SignUp.jsx';
+
+const Login = ({ onClose }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showSignUp, setShowSignUp] = useState(false);
+
+  const handleLogin = () => {
+    console.log("Username:", username);
+    console.log("Password:", password);
+
+    onClose();
+  };
+
+  const handleSignUpClick = () => {
+    setShowSignUp(true);
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center col">
+      <div className="flex w-full shadow-lg sm:w-3/4 lg:w-2/3 rounded-[2vh] overflow-hidden">
+        <div className="w-3/5 h-[80vh] bg-white">
+          <img src='https://github.com/namuagbea/cselective1-fullstack/blob/main/frontend/draft/media/login-photo.png?raw=true' alt="Login" className="object-cover min-w-full min-h-min mt-16 mr-2" />
+        </div>
+        <div className="w-2/3 flex-col bg-[#30689E]">
+          <div className='float-right pr-3 pt-3'>
+            <button onClick={onClose} className=" text-white hover:text-sky-200 p-2 focus:outline-none">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+          <div className='pt-14'>
+            <h2 className="text-[2rem] font-semibold text-center mt-12 text-white">Welcome!</h2>
+            <div className='px-14 mt-2'>
+              <div className="">  
+                <label className="text-base text-white font-normal " htmlFor="username">
+                  Username
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 mt-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="username"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  
+                />
+                
+                <div className="mb-4 mt-2">
+                  <label className="text-base font-normal text-white" htmlFor="password">
+                    Password
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 mt-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className='my-3'>
+                  <span className="text-sm text-white cursor-pointer">Forgot password?</span>
+
+                </div>
+
+                <button onClick={handleLogin} className="text-white bg-sky-900 hover:bg-sky-800 px-4 py-2 rounded-md mb-4 w-full">Login</button>
+                <div className="text-sm text-center flex items-center justify-center opacity-85 ">
+                  <hr className="flex-grow border-gray-300 border-t mr-2" />
+                  <p className=" text-white flex-grow-0 ">Don't have an account yet?</p>
+                  <hr className="flex-grow border-gray-300 border-t ml-2" />
+                </div>
+                <div className="text-center">
+                  <span className="text-sm opacity-85 mt-2 text-white cursor-pointer" onClick={handleSignUpClick}>Sign Up</span>
+                </div>
+                {showSignUp && <SignUp onClose={onClose} />}
+              </div>
+              
+            </div>
+
+          </div>
+
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
